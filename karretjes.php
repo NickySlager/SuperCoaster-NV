efd6162d483f9ff9c91df3b53276195f842d2c22
JorisdeVos <jorisdevos0@gmail.com>
2022-04-06 12:00:45 +0200

    minder stoorlijke achtergrond en text shadow borders

--- karretjes.php
+++ karretjes.php
@@ -1,18 +1,23 @@
 <body>
 	<div class="wrapper">
 		<?php require_once 'header.php';?>
+		<?php require_once 'head.php';?>
 	
 		<!-- afbeeldingen/placeholders -->
 		<div class="karretjesimg">
 			<img src="img/img8.jpg" />
 		</div>
 		<!-- kleine stukjes tekst van unique selling points karretjes -->
-		<p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. </p>
-		<p>Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren.</p>
+		<div class="textborder">
+			<p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. </p>
+			<p>Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren.</p>
+		</div>
 		<div class="karretjesimg">
 			<img src="img/img7.jpg" />
 		</div>
-		<p>Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia</p>
+		<div class="textborder">
+			<p>Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia</p>
+		</div>
 		<!-- gebruik display flex -->
 		<?php require_once 'footer.php';?>
 	</div>

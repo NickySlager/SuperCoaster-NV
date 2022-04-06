e1edb951e332652ebfeb4e0e6752a890db129f2c
JorisdeVos <jorisdevos0@gmail.com>
2022-04-06 11:43:52 +0200

    begin karretjes html en css

---
+++ karretjes.php
@@ -0,0 +1,19 @@
+<body>
+	<div class="wrapper">
+		<?php require_once 'header.php';?>
+	
+		<!-- afbeeldingen/placeholders -->
+		<div class="karretjesimg">
+			<img src="img/img8.jpg" />
+		</div>
+		<!-- kleine stukjes tekst van unique selling points karretjes -->
+		<p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. </p>
+		<p>Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren.</p>
+		<div class="karretjesimg">
+			<img src="img/img7.jpg" />
+		</div>
+		<p>Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia</p>
+		<!-- gebruik display flex -->
+		<?php require_once 'footer.php';?>
+	</div>
+</body>

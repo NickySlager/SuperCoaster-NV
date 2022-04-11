7502228360d3dc8525b8377a1e58bd9ba8a8925d
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-04-11 14:14:01 +0200

    contructie php en css stylin

---
+++ constructie.php
@@ -0,0 +1,29 @@
+<?php require_once("head.php") ?>
+<body>
+	<?php require_once("header.php") ?>
+	<!-- afbeeldingen/placeholders toevoegen -->
+	<div class="constructie">
+	<div class= "constructie-titel">
+	<h1>Constructie</h1>
+	</div>
+	<div class="constructie-banner">
+		<img src="img/img4.jpg" alt="achtbaan">
+	</div>
+	<div class ="constructie-container">
+	<img src="img/img1.jpg" alt="bouwplaatje">
+	<img src="img/img2.jpg" alt="bouwplaatje2">
+	</div>
+	<div class = "constructie-text">
+		<h3>Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is.
+			Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen.
+			Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan,
+			en langer meegaan dan andere stalen.</h3>
+		<h3>We hebben enorm geprobeerd de kosten zo laag mogelijk te houden,
+			dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies.
+			Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen.
+			Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</h3>
+	</div>
+</div>	   
+	<!-- kleine stukjes tekst met unique selling points constructie -->
+	<!-- gebruik display flex -->
+</body>

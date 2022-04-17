7cac1a1487957b6a4f93e272f21ff307070635b3
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-04-17 17:31:36 +0200

    footer + stylin css

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
-<div class="footerflex">
-    <nav>
-    <a href="index.php">Home</a>
-    <a href="constructie.php">Constructie</a>
-    <a href="karretjes.php">Karretjes</a>
-    <a href="Contact.php">Contact</a>
-    
-    </nav>
-    <p>2022 &#169;</p>
-</div>
+<footer>
+    <div class="footerflex">
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="constructie.php">Constructie</a>
+            <a href="karretjes.php">Karretjes</a>
+            <a href="Contact.php">Contact</a>
+        </nav>
+    </div>
+    <div class = "Copy">
+    <p>2022 &#169;, Joris,Pepijn,Nicky</p>
+    </div>
+</footer>

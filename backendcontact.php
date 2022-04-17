d46aa9292fc94e093d1d3f16ca1e4cfcd8554ad2
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-04-17 16:49:58 +0200

    backend contact aangepast nieuwe regel

--- backendcontact.php
+++ backendcontact.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $phoneNumber = $_POST['phoneNumber'];
 $email = $_POST['email'];
 
-file_put_contents("contact.txt", $name .", " . $phoneNumber . "," . $email ,FILE_APPEND);
+file_put_contents("contact.txt", $name .", " . $phoneNumber . "," . $email . "\n" ,FILE_APPEND);
 $msg = "<p> bedankt $name voor het invullen van het formulier!</p>";
 header("location: index.php?msg=$msg");
 ?>

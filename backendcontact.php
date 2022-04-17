5ccf039e35b18559f2e6fd3649c573b175463a6b
NickySlager <93587428+NickySlager@users.noreply.github.com>
2022-04-17 17:31:21 +0200

    backend checks toegevoegd

--- backendcontact.php
+++ backendcontact.php
@@ -2,7 +2,26 @@
 $name = $_POST['name'];
 $phoneNumber = $_POST['phoneNumber'];
 $email = $_POST['email'];
-
+if (empty ($name))
+{
+    $errors[] = "Vul een naam in";
+}
+if (empty ($email))
+{
+    $errors[] = "Vul een email in";
+}
+if (empty ($phoneNumber))
+{
+    $errors[] = "Vul een TelefoonNummer  in";
+}
+if (!is_numeric($phoneNumber))
+{
+    $errors[] = "Vul een nummer met getallen in";
+}
+if(isset($errors))
+{
+    die(var_dump($errors));
+}
 file_put_contents("contact.txt", $name .", " . $phoneNumber . "," . $email . "\n" ,FILE_APPEND);
 $msg = "<p> bedankt $name voor het invullen van het formulier!</p>";
 header("location: index.php?msg=$msg");
